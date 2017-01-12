937f3d4f8ff5bbe015d3eb89854933cc759b6cb5
Keven Jesus <falecom@keven.com.br>
2017-01-12 17:31:55 -0200

    atividade function

--- testes.js
+++ testes.js
@@ -1,27 +1,4 @@
 /*
-==================================
-COMO FUNCIONA COM JAVASCRIPT PURO
-==================================
-*/
+Funções
+================================ */
 
-// pegar ID
-var id = document.getElementById('teste').innerHTML;
-
-// pegar a classe
-var classe = document.getElementsByClassName("teste");
-
-// pegar elementos
-var elemento = document.getElementsByTagName("div");
-
-// pegar valores genericos
-var generico_classe = document.querySelector(".teste");
-var generico_id = document.querySelector("#teste");
-var generico_tag = document.querySelector("div");
-
-
-/*
-==================================
-COMO FUNCIONA COM JQUERY
-==================================
-*/
-var item = $(".imagem");
